9a48ef730f31c2673554cb7ef4bb737706871ea3
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-04-04 10:42:00 +0200

    Project setup continued

---
+++ src/connector01917/Constant.java
@@ -0,0 +1,16 @@
+package connector01917;
+
+
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+		server					= "sql-lab1.cc.dtu.dk",  // database-serveren
+		database				=  "sXXXXXX",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "sXXXXXX", // dit brugernavn = dit studienummer 
+		password				= ""; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 3306;
+}
