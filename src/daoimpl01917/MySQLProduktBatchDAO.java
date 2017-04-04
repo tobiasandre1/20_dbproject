289a2b3b25e7b08a513ebe98d6f5c8690dabb2eb
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-04-04 11:26:07 +0200

    Comments

--- src/daoimpl01917/MySQLProduktBatchDAO.java
+++ src/daoimpl01917/MySQLProduktBatchDAO.java
@@ -22,16 +22,34 @@ public class MySQLProduktBatchDAO implements ProduktBatchDAO {
 
 	@Override
 	public ProduktBatchDTO getProduktBatch(int pbId) throws DALException {
+		/*
+		 * We have imported our connector class. It's static, 
+		 * so we can use the methods within it without having to create an instance of it.
+		 * 
+		 * We can store the result of a query in the class ResultSet
+		 */
 		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatch WHERE pb_id = " + pbId);
-	    try {
+	    //Result is stored ^
+		try {
 	    	if (!rs.first()) throw new DALException("Produkt batch " + pbId + " findes ikke");
+	    	/*
+	    	 * If no rows are returned, 
+	    	 * that must mean that there is no batch with the given ID ^
+	    	 * We throw an exception because there is no object with the given ID.
+	    	 */
 	    	return new ProduktBatchDTO (rs.getInt("pb_id"), rs.getInt("status"), rs.getInt("recept_id"));
-	    }
+	    	//If there is a result returned, then we create a new object from it. ^
+		}
 	    catch (SQLException e) {throw new DALException(e); }
+		//We also check for SQL exceptions ^
 	}
 
 	@Override
 	public List<ProduktBatchDTO> getProduktBatchList() throws DALException {
+		/*
+		 * We return a list of all the product batches. 
+		 * Our query selects all present elements in the table.
+		 */
 		List<ProduktBatchDTO> list = new ArrayList<ProduktBatchDTO>();
 		ResultSet rs = Connector.doQuery("SELECT * FROM produktbatch");
 		try
@@ -47,6 +65,7 @@ public class MySQLProduktBatchDAO implements ProduktBatchDAO {
 
 	@Override
 	public void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+		//We do an insert query with the data in our DTO.
 		Connector.doUpdate(
 				"INSERT INTO produktbatch(pb_id, status, recept_id) VALUES " +
 				"(" + produktbatch.getPbId() + ", '" + produktbatch.getStatus() + 
@@ -57,6 +76,7 @@ public class MySQLProduktBatchDAO implements ProduktBatchDAO {
 
 	@Override
 	public void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
+		//We do an update query in the same way as our create method.
 		Connector.doUpdate(
 				"UPDATE produktbatch SET pb_id = " + produktbatch.getPbId()
 				+ ", status = " + produktbatch.getStatus()
