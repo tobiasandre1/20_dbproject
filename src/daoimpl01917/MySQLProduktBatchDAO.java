f24b364888d180316af47b2eeab4e599b782537e
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-04-06 22:49:30 +0200

    Fixed warning

--- src/daoimpl01917/MySQLProduktBatchDAO.java
+++ src/daoimpl01917/MySQLProduktBatchDAO.java
@@ -11,7 +11,6 @@ import java.util.List;
 import connector01917.Connector;
 import daointerfaces01917.DALException;
 import daointerfaces01917.ProduktBatchDAO;
-import dto01917.OperatoerDTO;
 import dto01917.ProduktBatchDTO;
 
 /**
