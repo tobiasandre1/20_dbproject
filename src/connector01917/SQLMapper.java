b073427fdaa0a48e23f3e1ee7fbaa6832fb58674
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-04-19 17:39:52 +0200

    Update of weird class. Do not mind. It is not used yet.

--- src/connector01917/SQLMapper.java
+++ src/connector01917/SQLMapper.java
@@ -2,6 +2,7 @@ package connector01917;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -17,6 +18,23 @@ public class SQLMapper {
 			String res = props.getProperty(Integer.toString(i));
 			in.close();
 			return res; 
+		} catch (IOException e) {
+			throw new IllegalStateException("Unable to load properties");
+		}
+	}
+	
+	public static String getStatement(String str){
+		
+		Properties props = new Properties();
+		try {
+			File file = new File("SQL.txt");
+			FileInputStream in = new FileInputStream(file);
+			props.load(in);
+			String res = props.getProperty(str);
+			in.close();
+			return res; 
+		} catch (FileNotFoundException e){
+			throw new IllegalStateException("SQL file does not exist");
 		} catch (IOException e) {
 			throw new IllegalStateException("Unable to load properties");
 		}
